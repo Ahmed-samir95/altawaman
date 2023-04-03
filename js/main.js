a411a8547ec4f88bad5e1c832937b541eb7a4648
ahmed-samir <ahmedsamirr126@gmail.com>
2023-04-03 14:26:52 +0200

    header section

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// show nav
+let menu = document.querySelector('.menu-icon')
+let nav = document.querySelector('.nav')
+console.log(nav)
+menu.onclick = () => {
+    nav.classList.toggle("open")
+    menu.classList.toggle("close")
+}
