df1ec49bd36e39a04dbe9a14036d11866540d740
ahmed-samir <ahmedsamirr126@gmail.com>
2023-04-06 21:26:56 +0200

    btn up

--- js/main.js
+++ js/main.js
@@ -6,3 +6,29 @@ menu.onclick = () => {
     nav.classList.toggle("open")
     menu.classList.toggle("close")
 }
+
+// btn to up
+
+let btn = document.querySelector('.btn-up')
+let header = document.querySelector('header')
+
+// show btn
+window.onscroll = function () {
+    if (window.scrollY >= 200) {
+        btn.style.display = 'block'
+        header.style.backgroundColor = 'rgba(12, 90, 219, 0.7)'
+
+    } else {
+        btn.style.display = 'none'
+        header.style.backgroundColor = 'transparent'
+
+    }
+}
+
+// move to up 
+btn.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
