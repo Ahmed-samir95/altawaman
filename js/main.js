0abda7d381eea3d6ace406646c745fd5c6651a02
ahmed-samir <ahmedsamirr126@gmail.com>
2023-04-07 22:23:42 +0200

    modified js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ let header = document.querySelector('header')
 
 // show btn
 window.onscroll = function () {
-    if (window.scrollY >= 200) {
+    if (window.scrollY >= 100) {
         btn.style.display = 'block'
         header.style.backgroundColor = 'rgba(12, 90, 219, 0.7)'
 
