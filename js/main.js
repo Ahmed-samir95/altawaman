39ea17504ff4effd89a26b91f736d48377670eb7
ahmed-samir <ahmedsamirr126@gmail.com>
2024-03-18 20:43:49 +0200

    modified nav section

--- js/main.js
+++ js/main.js
@@ -1,27 +1,24 @@
-// show nav
-let menu = document.querySelector('.menu-icon')
-let nav = document.querySelector('.nav')
-console.log(nav)
-menu.onclick = () => {
-    nav.classList.toggle("open")
-    menu.classList.toggle("close")
-}
+// // show nav
+// let menu = document.querySelector('.menu-icon')
+// let nav = document.querySelector('.nav')
+// console.log(nav)
+// menu.onclick = () => {
+//     nav.classList.toggle("open")
+//     menu.classList.toggle("close")
+// }
 
 // btn to up
 
 let btn = document.querySelector('.btn-up')
-let header = document.querySelector('header')
+let nav = document.querySelector('nav')
 
 // show btn
 window.onscroll = function () {
     if (window.scrollY >= 100) {
         btn.style.display = 'block'
-        header.style.backgroundColor = 'rgba(12, 90, 219, 0.7)'
 
     } else {
         btn.style.display = 'none'
-        header.style.backgroundColor = 'transparent'
-
     }
 }
 
